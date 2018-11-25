9b4e7c4f6e5fa5568f575c8b306e9eba5a00ebbe
SkyeBlack <296471989@qq.com>
2018-11-25 13:51:33 +0800

    font style change

--- EvaluationIndicatorSystem/UI/ChangePassword.Designer.cs
+++ EvaluationIndicatorSystem/UI/ChangePassword.Designer.cs
@@ -50,7 +50,7 @@
             this.btn_cancel.FlatAppearance.BorderSize = 0;
             this.btn_cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_cancel.ForeColor = System.Drawing.Color.White;
-            this.btn_cancel.Location = new System.Drawing.Point(325, 286);
+            this.btn_cancel.Location = new System.Drawing.Point(325, 265);
             this.btn_cancel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_cancel.Name = "btn_cancel";
             this.btn_cancel.Size = new System.Drawing.Size(80, 35);
@@ -64,7 +64,7 @@
             this.btn_ok.FlatAppearance.BorderSize = 0;
             this.btn_ok.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_ok.ForeColor = System.Drawing.Color.White;
-            this.btn_ok.Location = new System.Drawing.Point(84, 286);
+            this.btn_ok.Location = new System.Drawing.Point(85, 265);
             this.btn_ok.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_ok.Name = "btn_ok";
             this.btn_ok.Size = new System.Drawing.Size(80, 35);
@@ -75,27 +75,29 @@
             // 
             // txt_password
             // 
-            this.txt_password.Location = new System.Drawing.Point(185, 167);
+            this.txt_password.Font = new System.Drawing.Font("微软雅黑", 13F);
+            this.txt_password.Location = new System.Drawing.Point(185, 144);
             this.txt_password.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txt_password.Name = "txt_password";
             this.txt_password.PasswordChar = '*';
-            this.txt_password.Size = new System.Drawing.Size(220, 25);
+            this.txt_password.Size = new System.Drawing.Size(220, 30);
             this.txt_password.TabIndex = 10;
             // 
             // txt_name
             // 
             this.txt_name.Enabled = false;
-            this.txt_name.Location = new System.Drawing.Point(185, 47);
+            this.txt_name.Font = new System.Drawing.Font("微软雅黑", 13F);
+            this.txt_name.Location = new System.Drawing.Point(185, 34);
             this.txt_name.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txt_name.Name = "txt_name";
-            this.txt_name.Size = new System.Drawing.Size(220, 25);
+            this.txt_name.Size = new System.Drawing.Size(220, 30);
             this.txt_name.TabIndex = 9;
             // 
             // lbl_password
             // 
             this.lbl_password.AutoSize = true;
             this.lbl_password.BackColor = System.Drawing.Color.Transparent;
-            this.lbl_password.Location = new System.Drawing.Point(80, 170);
+            this.lbl_password.Location = new System.Drawing.Point(80, 150);
             this.lbl_password.Name = "lbl_password";
             this.lbl_password.Size = new System.Drawing.Size(62, 20);
             this.lbl_password.TabIndex = 8;
@@ -105,7 +107,7 @@
             // 
             this.lbl_name.AutoSize = true;
             this.lbl_name.BackColor = System.Drawing.Color.Transparent;
-            this.lbl_name.Location = new System.Drawing.Point(80, 50);
+            this.lbl_name.Location = new System.Drawing.Point(80, 40);
             this.lbl_name.Name = "lbl_name";
             this.lbl_name.Size = new System.Drawing.Size(62, 20);
             this.lbl_name.TabIndex = 7;
@@ -113,18 +115,19 @@
             // 
             // txt_password2
             // 
-            this.txt_password2.Location = new System.Drawing.Point(185, 227);
+            this.txt_password2.Font = new System.Drawing.Font("微软雅黑", 13F);
+            this.txt_password2.Location = new System.Drawing.Point(185, 199);
             this.txt_password2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txt_password2.Name = "txt_password2";
             this.txt_password2.PasswordChar = '*';
-            this.txt_password2.Size = new System.Drawing.Size(220, 25);
+            this.txt_password2.Size = new System.Drawing.Size(220, 30);
             this.txt_password2.TabIndex = 14;
             // 
             // lbl_password2
             // 
             this.lbl_password2.AutoSize = true;
             this.lbl_password2.BackColor = System.Drawing.Color.Transparent;
-            this.lbl_password2.Location = new System.Drawing.Point(80, 230);
+            this.lbl_password2.Location = new System.Drawing.Point(80, 205);
             this.lbl_password2.Name = "lbl_password2";
             this.lbl_password2.Size = new System.Drawing.Size(62, 20);
             this.lbl_password2.TabIndex = 13;
@@ -134,7 +137,7 @@
             // 
             this.lbl_pw_msg.AutoSize = true;
             this.lbl_pw_msg.ForeColor = System.Drawing.Color.Red;
-            this.lbl_pw_msg.Location = new System.Drawing.Point(185, 199);
+            this.lbl_pw_msg.Location = new System.Drawing.Point(70, 175);
             this.lbl_pw_msg.Name = "lbl_pw_msg";
             this.lbl_pw_msg.Size = new System.Drawing.Size(0, 20);
             this.lbl_pw_msg.TabIndex = 16;
@@ -143,25 +146,26 @@
             // 
             this.lbl_pw_msg2.AutoSize = true;
             this.lbl_pw_msg2.ForeColor = System.Drawing.Color.Red;
-            this.lbl_pw_msg2.Location = new System.Drawing.Point(185, 259);
+            this.lbl_pw_msg2.Location = new System.Drawing.Point(70, 230);
             this.lbl_pw_msg2.Name = "lbl_pw_msg2";
             this.lbl_pw_msg2.Size = new System.Drawing.Size(0, 20);
             this.lbl_pw_msg2.TabIndex = 17;
             // 
             // txt_prePassword
             // 
-            this.txt_prePassword.Location = new System.Drawing.Point(185, 107);
+            this.txt_prePassword.Font = new System.Drawing.Font("微软雅黑", 13F);
+            this.txt_prePassword.Location = new System.Drawing.Point(185, 89);
             this.txt_prePassword.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txt_prePassword.Name = "txt_prePassword";
             this.txt_prePassword.PasswordChar = '*';
-            this.txt_prePassword.Size = new System.Drawing.Size(220, 25);
+            this.txt_prePassword.Size = new System.Drawing.Size(220, 30);
             this.txt_prePassword.TabIndex = 19;
             // 
             // lbl_prePassword
             // 
             this.lbl_prePassword.AutoSize = true;
             this.lbl_prePassword.BackColor = System.Drawing.Color.Transparent;
-            this.lbl_prePassword.Location = new System.Drawing.Point(80, 110);
+            this.lbl_prePassword.Location = new System.Drawing.Point(80, 95);
             this.lbl_prePassword.Name = "lbl_prePassword";
             this.lbl_prePassword.Size = new System.Drawing.Size(62, 20);
             this.lbl_prePassword.TabIndex = 18;
@@ -171,7 +175,7 @@
             // 
             this.lbl_prePassword_msg.AutoSize = true;
             this.lbl_prePassword_msg.ForeColor = System.Drawing.Color.Red;
-            this.lbl_prePassword_msg.Location = new System.Drawing.Point(185, 140);
+            this.lbl_prePassword_msg.Location = new System.Drawing.Point(70, 115);
             this.lbl_prePassword_msg.Name = "lbl_prePassword_msg";
             this.lbl_prePassword_msg.Size = new System.Drawing.Size(0, 20);
             this.lbl_prePassword_msg.TabIndex = 20;
@@ -181,7 +185,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(503, 362);
+            this.ClientSize = new System.Drawing.Size(494, 322);
             this.Controls.Add(this.lbl_prePassword_msg);
             this.Controls.Add(this.txt_prePassword);
             this.Controls.Add(this.lbl_prePassword);
--- EvaluationIndicatorSystem/UI/UserRegister.Designer.cs
+++ EvaluationIndicatorSystem/UI/UserRegister.Designer.cs
@@ -47,7 +47,7 @@
             this.btn_cancel.FlatAppearance.BorderSize = 0;
             this.btn_cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_cancel.ForeColor = System.Drawing.Color.White;
-            this.btn_cancel.Location = new System.Drawing.Point(325, 226);
+            this.btn_cancel.Location = new System.Drawing.Point(328, 216);
             this.btn_cancel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_cancel.Name = "btn_cancel";
             this.btn_cancel.Size = new System.Drawing.Size(80, 35);
@@ -62,7 +62,7 @@
             this.btn_ok.FlatAppearance.BorderSize = 0;
             this.btn_ok.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_ok.ForeColor = System.Drawing.Color.White;
-            this.btn_ok.Location = new System.Drawing.Point(84, 226);
+            this.btn_ok.Location = new System.Drawing.Point(87, 216);
             this.btn_ok.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_ok.Name = "btn_ok";
             this.btn_ok.Size = new System.Drawing.Size(80, 35);
@@ -74,7 +74,7 @@
             // txt_password
             // 
             this.txt_password.Font = new System.Drawing.Font("微软雅黑", 13F);
-            this.txt_password.Location = new System.Drawing.Point(185, 104);
+            this.txt_password.Location = new System.Drawing.Point(188, 94);
             this.txt_password.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txt_password.Name = "txt_password";
             this.txt_password.PasswordChar = '*';
@@ -84,7 +84,7 @@
             // txt_name
             // 
             this.txt_name.Font = new System.Drawing.Font("微软雅黑", 13F);
-            this.txt_name.Location = new System.Drawing.Point(185, 44);
+            this.txt_name.Location = new System.Drawing.Point(188, 34);
             this.txt_name.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txt_name.Name = "txt_name";
             this.txt_name.Size = new System.Drawing.Size(220, 30);
@@ -94,7 +94,7 @@
             // 
             this.lbl_password.AutoSize = true;
             this.lbl_password.BackColor = System.Drawing.Color.Transparent;
-            this.lbl_password.Location = new System.Drawing.Point(80, 110);
+            this.lbl_password.Location = new System.Drawing.Point(83, 100);
             this.lbl_password.Name = "lbl_password";
             this.lbl_password.Size = new System.Drawing.Size(48, 20);
             this.lbl_password.TabIndex = 8;
@@ -104,7 +104,7 @@
             // 
             this.lbl_name.AutoSize = true;
             this.lbl_name.BackColor = System.Drawing.Color.Transparent;
-            this.lbl_name.Location = new System.Drawing.Point(80, 50);
+            this.lbl_name.Location = new System.Drawing.Point(83, 40);
             this.lbl_name.Name = "lbl_name";
             this.lbl_name.Size = new System.Drawing.Size(62, 20);
             this.lbl_name.TabIndex = 7;
@@ -113,7 +113,7 @@
             // txt_password2
             // 
             this.txt_password2.Font = new System.Drawing.Font("微软雅黑", 13F);
-            this.txt_password2.Location = new System.Drawing.Point(185, 164);
+            this.txt_password2.Location = new System.Drawing.Point(188, 154);
             this.txt_password2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txt_password2.Name = "txt_password2";
             this.txt_password2.PasswordChar = '*';
@@ -124,7 +124,7 @@
             // 
             this.lbl_password2.AutoSize = true;
             this.lbl_password2.BackColor = System.Drawing.Color.Transparent;
-            this.lbl_password2.Location = new System.Drawing.Point(80, 170);
+            this.lbl_password2.Location = new System.Drawing.Point(83, 160);
             this.lbl_password2.Name = "lbl_password2";
             this.lbl_password2.Size = new System.Drawing.Size(48, 20);
             this.lbl_password2.TabIndex = 13;
@@ -134,7 +134,7 @@
             // 
             this.lbl_name_msg.AutoSize = true;
             this.lbl_name_msg.ForeColor = System.Drawing.Color.Red;
-            this.lbl_name_msg.Location = new System.Drawing.Point(70, 70);
+            this.lbl_name_msg.Location = new System.Drawing.Point(73, 60);
             this.lbl_name_msg.Name = "lbl_name_msg";
             this.lbl_name_msg.Size = new System.Drawing.Size(0, 20);
             this.lbl_name_msg.TabIndex = 15;
@@ -143,7 +143,7 @@
             // 
             this.lbl_pw_msg.AutoSize = true;
             this.lbl_pw_msg.ForeColor = System.Drawing.Color.Red;
-            this.lbl_pw_msg.Location = new System.Drawing.Point(70, 130);
+            this.lbl_pw_msg.Location = new System.Drawing.Point(73, 120);
             this.lbl_pw_msg.Name = "lbl_pw_msg";
             this.lbl_pw_msg.Size = new System.Drawing.Size(0, 20);
             this.lbl_pw_msg.TabIndex = 16;
@@ -152,7 +152,7 @@
             // 
             this.lbl_pw_msg2.AutoSize = true;
             this.lbl_pw_msg2.ForeColor = System.Drawing.Color.Red;
-            this.lbl_pw_msg2.Location = new System.Drawing.Point(70, 190);
+            this.lbl_pw_msg2.Location = new System.Drawing.Point(73, 180);
             this.lbl_pw_msg2.Name = "lbl_pw_msg2";
             this.lbl_pw_msg2.Size = new System.Drawing.Size(0, 20);
             this.lbl_pw_msg2.TabIndex = 17;
@@ -162,7 +162,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(503, 303);
+            this.ClientSize = new System.Drawing.Size(494, 287);
             this.Controls.Add(this.lbl_pw_msg2);
             this.Controls.Add(this.lbl_pw_msg);
             this.Controls.Add(this.lbl_name_msg);
